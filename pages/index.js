424def1f2795fdcf29c3dbbeada75813287deb6f
Pekka Kokkonen <pekka.kokkonen@vincit.fi>
2020-10-19 14:36:35 +0300

    colors

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
   }, [])
 
   return (
-    <div style={{width: "100vw", height: "100vh", display: "flex", flexDirection: "column", justifyContent:"center", alignItems: "center"}}>
+    <div style={{width: "100vw", height: "100vh", display: "flex", flexDirection: "column", justifyContent:"center", alignItems: "center", backgroundColor: "black", color: "white"}}>
       <h1 style={{ fontSize: "calc(100vh * 0.4)", margin: "0 auto" }}>{format(currentTime, "HH:mm:ss")}</h1>
       <h2 style={{ margin: "0 auto" }}>{format(currentTime, "EE dd.MM.yyyy")}</h2>
     </div>
