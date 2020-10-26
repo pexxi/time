b6c8aa7ce066a64efb4a31503eb3ce8bd371584e
Pekka Kokkonen <pekka.kokkonen@vincit.fi>
2020-10-26 08:54:59 +0200

    Reafctor output, styles

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
 import React, {useState, useEffect} from "react";
 import format from "date-fns/format";
+import styles from './Home.module.css';
 
 export default function Home() {
 
-  const [currentTime, setCurrentTime] = useState(new Date());
+  const [currentTime, setCurrentTime] = useState("");
+  const [currentDate, setCurrentDate] = useState("");
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentTime(new Date())
-    }, 1000);
+      const now = new Date();
+      setCurrentTime(format(now, "HH:mm"));
+      setCurrentDate(format(now, "EE dd.MM.yyyy"));
+    }, 60000);
     return () => {
       clearInterval(interval);
     }
   }, [])
 
   return (
-    <div style={{width: "100vw", height: "100vh", display: "flex", flexDirection: "column", justifyContent:"center", alignItems: "center", backgroundColor: "black", color: "white"}}>
-      <h1 style={{ fontSize: "calc(100vh * 0.4)", margin: "0 auto" }}>{format(currentTime, "HH:mm:ss")}</h1>
-      <h2 style={{ margin: "0 auto" }}>{format(currentTime, "EE dd.MM.yyyy")}</h2>
+    <div className={styles.container}>
+      <h1 className={styles.time}>{currentTime}</h1>
+      <h2 className={styles.date}>{currentDate}</h2>
     </div>
   )
 }
